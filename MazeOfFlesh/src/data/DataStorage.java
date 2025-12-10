ac563590efead689cdbc6bdbbf666fdda3a757f4
StrayNight <sabawp11@gmail.com>
2025-12-10 14:41:39 +0800

    Add files via upload

--- MazeOfFlesh/src/data/DataStorage.java
+++ MazeOfFlesh/src/data/DataStorage.java
@@ -3,7 +3,7 @@ package data;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class DataStorage implements Serializable{
+public class DataStorage implements Serializable {
 
 	// Player Stats
 	int level;
@@ -16,12 +16,12 @@ public class DataStorage implements Serializable{
 	int exp;
 	int nextExp;
 	int coin;
-	
+
 	// Player Inventory
 	ArrayList<String> itemNames = new ArrayList<>();
 	ArrayList<Integer> itemAmounts = new ArrayList<>();
-	
-	//Object on map
+
+	// Object on map
 	String mapObjectNames[][];
 	int mapObjectWorldX[][];
 	int mapObjectWorldY[][];
--- MazeOfFlesh/src/data/SaveLoad.java
+++ MazeOfFlesh/src/data/SaveLoad.java
@@ -5,127 +5,106 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-
-import entity.Entity;
 import main.GamePanel;
-import object.OBJ_Shards;
 
 public class SaveLoad {
 
 	GamePanel gp;
-	
+
 	public SaveLoad(GamePanel gp) {
 		this.gp = gp;
 	}
-	
+
 	public void save() {
-		
+
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("save.dat")));
-			
+
 			DataStorage ds = new DataStorage();
-			
+
 			// Player Stats
-			ds.level = gp.player.level;
 			ds.maxLife = gp.player.maxLife;
 			ds.life = gp.player.life;
-			ds.maxMana = gp.player.maxMana;
-			ds.mana = gp.player.mana;
-			ds.strength = gp.player.strength;
-			ds.dexterity = gp.player.dexterity;
-			ds.exp = gp.player.exp;
-			ds.nextExp = gp.player.nextExp;
-			ds.coin = gp.player.coin;
-			
+
 			// Player Inventory
-			for(int i = 0; i < gp.player.inventory.size(); i++) {
+			for (int i = 0; i < gp.player.inventory.size(); i++) {
 				ds.itemNames.add(gp.player.inventory.get(i).name);
 				ds.itemAmounts.add(gp.player.inventory.get(i).amount);
 			}
-			
+
 			// Object on maps
 			ds.mapObjectNames = new String[gp.maxMap][gp.obj[1].length];
 			ds.mapObjectWorldX = new int[gp.maxMap][gp.obj[1].length];
 			ds.mapObjectWorldY = new int[gp.maxMap][gp.obj[1].length];
 			ds.mapObjectLootNames = new String[gp.maxMap][gp.obj[1].length];
 			ds.mapObjectOpened = new boolean[gp.maxMap][gp.obj[1].length];
-			
-			for(int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
-				
-				for(int i = 0; i < gp.obj[1].length; i++) {
-					
-					if(gp.obj[mapNum][i] == null) {
+
+			for (int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
+
+				for (int i = 0; i < gp.obj[1].length; i++) {
+
+					if (gp.obj[mapNum][i] == null) {
 						ds.mapObjectNames[mapNum][i] = "NA";
-					}
-					else {
+					} else {
 						ds.mapObjectNames[mapNum][i] = gp.obj[mapNum][i].name;
 						ds.mapObjectWorldX[mapNum][i] = gp.obj[mapNum][i].worldX;
 						ds.mapObjectWorldY[mapNum][i] = gp.obj[mapNum][i].worldY;
-						if(gp.obj[mapNum][i].loot != null) {
+						if (gp.obj[mapNum][i].loot != null) {
 							ds.mapObjectLootNames[mapNum][i] = gp.obj[mapNum][i].loot.name;
 						}
 						ds.mapObjectOpened[mapNum][i] = gp.obj[mapNum][i].opened;
 					}
 				}
 			}
-			
+
 			// Write the Data Storage Update
 			oos.writeObject(ds);
-			
-			
+
 		} catch (Exception e) {
 			System.out.println("Save Exception!!");
-		} 
+		}
 	}
+
 	public void load() {
-		
+
 		try {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("save.dat")));
-			
+
 			// Read Data Storage
-			DataStorage ds = (DataStorage)ois.readObject();
-			
-			gp.player.level = ds.level;
+			DataStorage ds = (DataStorage) ois.readObject();
+
 			gp.player.maxLife = ds.maxLife;
 			gp.player.life = ds.life;
-			gp.player.maxMana = ds.maxMana;
-			gp.player.mana = ds.mana;
-			gp.player.strength = ds.strength;
-			gp.player.dexterity = ds.dexterity;
-			gp.player.exp = ds.exp;
-			gp.player.nextExp = ds.nextExp;
-			gp.player.coin = ds.coin;
-			
+
 			// Player Invetory
 			gp.player.inventory.clear();
-			for(int i = 0; i < ds.itemNames.size(); i++) {
+			for (int i = 0; i < ds.itemNames.size(); i++) {
 				gp.player.inventory.add(gp.eGenerator.getObject(ds.itemNames.get(i)));
 				gp.player.inventory.get(i).amount = ds.itemAmounts.get(i);
 			}
-			
+
 			// Object on maps
-			for(int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
-				
-				for(int i = 0; i < gp.obj[1].length; i++) {
-					
-					if(ds.mapObjectNames[mapNum][i].equals("NA")) {
+			for (int mapNum = 0; mapNum < gp.maxMap; mapNum++) {
+
+				for (int i = 0; i < gp.obj[1].length; i++) {
+
+					if (ds.mapObjectNames[mapNum][i].equals("NA")) {
 						gp.obj[mapNum][i] = null;
-					}
-					else {
-						gp.obj[mapNum][i] = gp.eGenerator.getObject( ds.mapObjectNames[mapNum][i]);
+					} else {
+						gp.obj[mapNum][i] = gp.eGenerator.getObject(ds.mapObjectNames[mapNum][i]);
 						gp.obj[mapNum][i].worldX = ds.mapObjectWorldX[mapNum][i];
 						gp.obj[mapNum][i].worldY = ds.mapObjectWorldY[mapNum][i];
-						if(ds.mapObjectLootNames[mapNum][i] != null) {
+						if (ds.mapObjectLootNames[mapNum][i] != null) {
 							gp.obj[mapNum][i].loot = gp.eGenerator.getObject(ds.mapObjectLootNames[mapNum][i]);
 						}
 						gp.obj[mapNum][i].opened = ds.mapObjectOpened[mapNum][i];
-						if(gp.obj[mapNum][i].opened == true) {
+						if (gp.obj[mapNum][i].opened == true) {
 							gp.obj[mapNum][i].down1 = gp.obj[mapNum][i].image2;
 						}
 					}
 				}
 			}
-			
+
 		} catch (Exception e) {
 			System.out.println("Load Exception!!");
 		}
